8ae7dd8d2efd532f617793aa6d30e321985f5bac
aqsaldpa <aqsaldpa28@gmail.com>
2022-01-01 00:53:35 +0700

    updt

--- app/src/main/java/com/example/fp/ProfileActivity.java
+++ app/src/main/java/com/example/fp/ProfileActivity.java
@@ -1,36 +1,163 @@
 package com.example.fp;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.fragment.app.FragmentManager;
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.blikoon.qrcodescanner.QrCodeActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 public class ProfileActivity extends AppCompatActivity {
-    Button logout;
+    private static final int REQUEST_CODE_QR_SCAN = 101;
+    BottomNavigationView bottomnav;
+    FragmentManager fragmentManager;
+    Button logout,bayar;
     TextView tvUsername, tvEMail;
-
     FirebaseAuth mAuth;
+    Button scan;
+    String channelnotif = "channelku";
+    String channelid = "default";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        mAuth = FirebaseAuth.getInstance();
-        logout = findViewById(R.id.btnLogout);
-        tvUsername = findViewById(R.id.tvUsername);
-        tvEMail = findViewById(R.id.tvEmail);
 
+        mAuth = FirebaseAuth.getInstance();
+        tvUsername = findViewById(R.id.tvUsername);
+
+        bayar = findViewById(R.id.btnbayar);
+
+        bayar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(ProfileActivity.this,"Testing Notif");
+                builder.setContentTitle("Pembayaran anda telah sukses !");
+                builder.setContentText("Selamat!!!");
+                builder.setSmallIcon(R.drawable.ic_baseline_check_24);
+                builder.setAutoCancel(true);
+
+                NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                    int importance = NotificationManager.IMPORTANCE_HIGH;
+                    NotificationChannel notificationChannel = new
+                            NotificationChannel(channelnotif, "contoh channel", importance);
+                    notificationChannel.enableLights(true);
+                    notificationChannel.setLightColor(Color.RED);
+                    builder.setChannelId(channelnotif);
+                    assert mNotificationManager != null;
+                    mNotificationManager.createNotificationChannel(notificationChannel);
+                }
+                assert mNotificationManager != null;
+                mNotificationManager.notify((int) System.currentTimeMillis(), builder.build());
+
+            }
+        });
+
+        scan = findViewById(R.id.btnScan);
+        scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dexter.withContext(getApplicationContext())
+                        .withPermission(Manifest.permission.CAMERA)
+                        .withListener(new PermissionListener() {
+                            @Override
+                            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                                Intent i = new Intent(ProfileActivity.this, QrCodeActivity.class);
+                                startActivityForResult( i,REQUEST_CODE_QR_SCAN);
+                            }
+
+                            @Override
+                            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                                permissionDeniedResponse.getRequestedPermission();
+                            }
+
+                            @Override
+                            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+
+                            }
+                        }).check();
+            }
+        });
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != Activity.RESULT_OK) {
+            if (data == null)
+                return;
+            //Getting the passed result
+            String result = data.getStringExtra("com.blikoon.qrcodescanner.error_decoding_image");
+            if (result != null) {
+                AlertDialog alertDialog = new AlertDialog.Builder(ProfileActivity.this).create();
+                alertDialog.setTitle("Scan Error");
+                alertDialog.setMessage("QR Gagal Melakukan Scan");
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialog.show();
+            }
+            return;
+
+        }
+        if (requestCode == REQUEST_CODE_QR_SCAN) {
+            if (data == null)
+                return;
+            //Getting the passed result
+            String result = data.getStringExtra("com.blikoon.qrcodescanner.got_qr_scan_relult");
+            AlertDialog alertDialog = new AlertDialog.Builder(ProfileActivity.this).create();
+            alertDialog.setTitle("Hasil Scan");
+            alertDialog.setMessage(result);
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            alertDialog.show();
+
+        }
+        logout = findViewById(R.id.btnLogout);
         logout.setOnClickListener(v -> {
 
-            Toast.makeText(this, "LOG OUT Berhasil!!!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Logout Berhasil", Toast.LENGTH_SHORT).show();
             Intent i = new Intent(ProfileActivity.this, MainActivity.class);
             startActivity(i);
         });
+
+
+
 
     }
 }
